86a9b1208da3a0865f4efc10974b2b60b4dfb609
EmilioMendozaCortes <emiliomendocor@gmail.com>
2024-06-20 11:43:46 -0600

    Inserción de usuarios en un array

--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -1,8 +1,28 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
 
 user = APIRouter()
+users = []
+
+# @userModel
+class models_users(BaseModel):
+    id:str
+    usuarios:str
+    contrasena:str
+    created_at:datetime = datetime.now()
+    estatus:bool=False
+
+@user.get('/')
+def bienvenido():
+    return 'Bienvenido al sistema de APIs'
 
 @user.get('/users')
+def get_usuarios():
+    return users
 
-def helloworld():
-    return 'Hola  9B'
+@user.post('/users')
+def save_users(insert_users:models_users):
+    users.append(insert_users)
+    print(users)
+    return 'Datos guardados'
