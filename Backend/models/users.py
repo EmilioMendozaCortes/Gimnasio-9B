e61632510e3202ed3d8ad4e6005d1681c1bc67dd
EmilioMendozaCortes <emiliomendocor@gmail.com>
2024-06-27 10:55:43 -0600

    Creación del modelo de users

---
+++ Backend/models/users.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column,Boolean, Integer, String, DateTime
+from config.db import Base
+
+class users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    usuario = Column(String(255), index=True)
+    password= Column(String(255), index=True)
+    estatus = Column(Boolean, index=True)
