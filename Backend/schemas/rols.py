71cd67c16a1a6b92c6aa4544b14a9a0d16bdc51b
EmilioMendozaCortes <emiliomendocor@gmail.com>
2024-07-09 11:05:02 -0600

    actualización de los roles

--- Backend/schemas/rols.py
+++ Backend/schemas/rols.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class RolBase(BaseModel):
     Nombre:str
     Descripcion:str
-    Contrasena:str
+    # Contrasena:str
     Estatus:bool
     Fecha_Registro: datetime
     Fecha_Actualizacion: datetime
