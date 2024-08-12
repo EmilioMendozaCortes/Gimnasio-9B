73fd445b6b16af686e1671758034eeeec6f323e1
EmilioMendozaCortes <emiliomendocor@gamil.com>
2024-08-12 14:23:31 -0600

    integración de la tabla de dietas

--- Backend/app.py
+++ Backend/app.py
@@ -3,10 +3,12 @@ from routes.user import user
 from routes.person import person
 from routes.rol import rol
 from routes.usersrols import userrol
+from routes.dietas import dieta
 
 app = FastAPI()
 app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
+app.include_router(dieta)
 print("Hola bienvenido a mi backend")
